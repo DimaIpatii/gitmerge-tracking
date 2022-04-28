a8c2978e8c40145ea5c4a77a577774238a683780
Dmytro Ipatii <dmytro.ipatii@aalto.it>
2022-04-28 14:58:24 +0200

    message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import {HOME_URL, GALLERY_URL} from './shared/urls';
+import {HOME_URL, GALLERY_URL, FORM_URL} from './shared/urls';
 
 const homeLink = document.createElement("a");
 homeLink.setAttribute("href", HOME_URL);
 
 const galleryLink = document.createElement("a");
 galleryLink.setAttribute("href", GALLERY_URL);
+
+const formLink = document.createElement("a");
+formLink.setAttribute("href", FORM_URL);
