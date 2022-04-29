85708c8025a27d013cb3b3a94789f42fcc2f6739
Dmytro Ipatii <dmytro.ipatii@aalto.it>
2022-04-29 16:51:06 +0200

    message

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -3,4 +3,4 @@ const GALLERY_URL = "https://STG-gallery.com";
 const FORM_URL = "https://STG-form.com";
 const SHOP_URL = "https://STG-shop.com";
 const ACCOUT_URL = "https://STG-account.com";
-const OTHER_URL = "https://STG-other.it";
+const OTHER_URL = "https://STG-other.com";
