4b3983241172243018b51869eed5f73d732b1d4b
Dmytro Ipatii <dmytro.ipatii@aalto.it>
2022-04-29 17:23:17 +0200

    message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {HOME_URL, GALLERY_URL, FORM_URL, SHOP_URL} from './shared/urls';
+import {userName} from './shared/variables';
 
 const homeLink = document.createElement("a");
 homeLink.setAttribute("href", HOME_URL);
@@ -11,3 +12,5 @@ formLink.setAttribute("href", FORM_URL);
 
 const shopLink = document.createElement("a");
 shopLink.setAttribute("href", SHOP_URL);
+
+console.log(userName);
